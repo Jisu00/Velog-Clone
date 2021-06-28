11ec00af0840d365bc34f3cfd929dc7f0e50673f
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-06-28 11:09:54 +0900

    Feat(routers): Set routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import Home from "pages/Home";
+import LoginInfo from "pages/LoginInfo";
+import Header from "components/Header";
+// import LoginPopup from "components/LoginPopup";
+import GlobalStyles from "styles/GlobalStyles";
 
 function App() {
-  return (
+	return (
     <div className="App">
-
-    </div>
-  );
+      <GlobalStyles/>
+      <Header></Header>
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/login-info" exact component={LoginInfo} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
