18a9491b239cac9df561ebb8840ced208fba9e3e
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-06-29 19:42:09 +0900

    Feat(login-popup): Set base code of popup

--- src/components/LoginPopup.jsx
+++ src/components/LoginPopup.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import deleteIcon from "assets/images/deleteIcon.svg";
+import githubIcon from "assets/images/githubIcon.png";
+import googleIcon from "assets/images/googleIcon.png";
+import config from "config";
 
 const PopupWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	position: fixed; /* Stay in place */
 	z-index: 1; /* Sit on top */
-	padding-top: 100px; /* Location of the box */
 	left: 0;
 	top: 0;
 	width: 100%; /* Full width */
@@ -16,9 +21,9 @@ const PopupWrapper = styled.div`
 
 const Popup = styled.div`
 	display: flex;
-  justify-content: center;
-  align-items: center;
-	height: 432px;
+	justify-content: center;
+	align-items: center;
+	height: 480px;
 `;
 
 const PopupLeftWrapper = styled.div`
@@ -27,7 +32,7 @@ const PopupLeftWrapper = styled.div`
 	justify-content: center;
 	align-items: center;
 	background-color: rgb(241, 243, 245);
-  height: 432px;
+	height: 480px;
 	padding: 24px;
 `;
 
@@ -44,18 +49,17 @@ const LoginGreetingText = styled.span`
 `;
 
 const PopupRightWrapper = styled.div`
-  height: 432px;
+	height: 480px;
 	padding: 24px;
-  background-color: #fff;
+	background-color: #fff;
 `;
 
 const ExitButtonWrapper = styled.div`
 	display: flex;
-  justify-content: flex-end;
+	justify-content: flex-end;
 `;
 
 const ExitButton = styled.button`
-
 	border: none;
 
 	background-color: transparent;
@@ -63,6 +67,13 @@ const ExitButton = styled.button`
 	&:hover {
 		cursor: pointer;
 	}
+`;
+
+const LoginAreaWrapper = styled.div`
+	display: flex;
+	height: 85%;
+	flex-direction: column;
+	justify-content: space-between;
 `;
 
 const LoginArea = styled.div``;
@@ -101,7 +112,49 @@ const LoginButton = styled.button`
 	width: 96px;
 `;
 
+const SocialArea = styled.div`
+	margin-top: 40px;
+`;
+
+const SocialButtonArea = styled.div`
+	display: flex;
+	justify-content: space-around;
+`;
+
+const IconButton = styled.button`
+	background: transparent;
+	border: none;
+`;
+
+const FooterAreaWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`;
+
+const FooterArea = styled.span``;
+
+const IsMemberText = styled.span`
+	margin-right: 4px;
+`;
+
+const MemberLink = styled.a`
+	color: rgb(18, 184, 134);
+	font-weight: bold;
+	&:hover {
+		text-decoration: underline;
+	}
+`;
+
 export default function LoginPopup({ isOpen, setIsLoginPopup }) {
+	const [isLogin, setIsLogin] = useState(true);
+	const GithubRoute = () => {
+		window.location.href = `https://github.com/login/oauth/authorize?client_id=${config.GITHUB_CLIENT_ID}&redirect_uri=http://localhost:3001`;
+	};
+
+	const toggleState = () => {
+		setIsLogin(!isLogin);
+	}
+
 	return (
 		<>
 			{isOpen && (
@@ -111,6 +164,7 @@ export default function LoginPopup({ isOpen, setIsLoginPopup }) {
 							<LoginImg></LoginImg>
 							<LoginGreetingText>환영합니다!</LoginGreetingText>
 						</PopupLeftWrapper>
+
 						<PopupRightWrapper>
 							<ExitButtonWrapper>
 								<ExitButton onClick={() => setIsLoginPopup(false)}>
@@ -118,15 +172,49 @@ export default function LoginPopup({ isOpen, setIsLoginPopup }) {
 								</ExitButton>
 							</ExitButtonWrapper>
 
-							<LoginArea>
-								<LoginText>로그인</LoginText>
-								<LoginTypeText>이메일로 로그인</LoginTypeText>
-								<EmailArea>
-									<EmailInput></EmailInput>
-									<LoginButton>로그인</LoginButton>
-								</EmailArea>
-								<LoginTypeText>소셜 계정으로 로그인</LoginTypeText>
-							</LoginArea>
+							<LoginAreaWrapper>
+								<LoginArea>
+									<LoginText>{isLogin ? "로그인" : "회원가입"}</LoginText>
+									<EmailArea>
+										<LoginTypeText>이메일로 {isLogin ? "로그인" : "회원가입"}</LoginTypeText>
+										<EmailInput></EmailInput>
+										<LoginButton>{isLogin ? "로그인" : "회원가입"}</LoginButton>
+									</EmailArea>
+									<SocialArea>
+										<LoginTypeText>소셜 계정으로 {isLogin ? "로그인" : "회원가입"}</LoginTypeText>
+										<SocialButtonArea>
+											<IconButton onClick={GithubRoute}>
+												<img
+													src={githubIcon}
+													alt="github login icon"
+													style={{
+														width: "48px",
+														height: "48px",
+														borderRadius: "50%",
+													}}
+												/>
+											</IconButton>
+											<IconButton>
+												<img
+													src={googleIcon}
+													alt="google login icon"
+													style={{
+														width: "48px",
+														height: "48px",
+														borderRadius: "50%",
+													}}
+												/>
+											</IconButton>
+										</SocialButtonArea>
+									</SocialArea>
+								</LoginArea>
+								<FooterAreaWrapper>
+									<FooterArea>
+										<IsMemberText>{isLogin ? "아직 회원이 아니신가요?" : "계정이 이미 있으신가요?"}</IsMemberText>
+										<MemberLink onClick={toggleState}>{isLogin ? "회원가입" : "로그인"}</MemberLink>
+									</FooterArea>
+								</FooterAreaWrapper>
+							</LoginAreaWrapper>
 						</PopupRightWrapper>
 					</Popup>
 				</PopupWrapper>
